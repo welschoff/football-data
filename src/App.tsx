24ff41d8d04117dcd3d718118aae467c9de0bd03
welschoff <h.welschoff@gmx.de>
2022-01-25 15:59:38 +0100

    Create Countrylist

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-import { useEffect, useState } from 'react';
+import Countries from './pages/Countries';
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    async function doFetch() {
-      const response = await fetch('/api/hello');
-      const result = await response.json();
-      setMessage(result.message);
-    }
-    doFetch();
-  }, []);
-
-  return (
-    <div>
-      <p>Hello Vite + React!</p>
-      <p>{message}</p>
-    </div>
-  );
+  return <Countries />;
 }
 
 export default App;
---
+++ src/pages/Countries.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import CountryCard, { CountryCardProps } from '../components/CountryCard';
+
+function Countries() {
+  const [countries, setCountries] = useState<CountryCardProps[] | null>([]);
+
+  const getCountries = async () => {
+    const response = await fetch(
+      'https://api-football-v1.p.rapidapi.com/v3/countries',
+      {
+        headers: {
+          'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
+          'x-rapidapi-key':
+            '7f76996b93msh5a4e487e8dd7106p1eddf8jsne1ae9ca636fa',
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data.response);
+    setCountries(data.response);
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  return (
+    <div>
+      {countries?.map((country) => (
+        <CountryCard
+          key={country.code}
+          flag={country.flag}
+          name={country.name}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Countries;
