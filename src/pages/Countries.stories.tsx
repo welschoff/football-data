ce7ef453f51a0b4d4d6a290e1a33f5e509b32080
welschoff <h.welschoff@gmx.de>
2022-01-27 13:20:51 +0100

    Add Countrylist

---
+++ src/pages/Countries.stories.tsx
@@ -0,0 +1,11 @@
+import Countries from './Countries';
+
+export default {
+  component: Countries,
+  title: 'Pages/Countries',
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = () => <Countries />;
