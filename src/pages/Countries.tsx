f480adbf1178b381f19e803fb44ecd988d96f610
welschoff <h.welschoff@gmx.de>
2022-01-27 14:46:38 +0100

    Add a Searchbar to filter Countries by name

--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -1,33 +1,52 @@
 import { useEffect, useState } from 'react';
 import CountryCard, { CountryCardProps } from '../components/CountryCard';
-import Search from '../components/Search';
+import { FaSearch } from 'react-icons/fa';
+import styles from './Countries.module.css';
 
 function Countries() {
-  const [countries, setCountries] = useState<CountryCardProps[] | null>([]);
-
-  const getCountries = async () => {
-    const response = await fetch('http://localhost:3001/api/countries');
-    const data = await response.json();
-    console.log(data);
-    setCountries(data);
-  };
+  const [countries, setCountries] = useState<CountryCardProps[]>([]);
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState<CountryCardProps[]>([]);
 
   useEffect(() => {
+    const getCountries = async () => {
+      const response = await fetch('http://localhost:3001/api/countries');
+      const data = await response.json();
+      console.log(data);
+      setCountries(data);
+    };
     getCountries();
   }, []);
 
+  useEffect(() => {
+    setOutput([]);
+    countries?.filter((value) => {
+      if (value.name.toLocaleLowerCase().includes(input.toLocaleLowerCase())) {
+        setOutput((output) => [...output, value]);
+      }
+    });
+  }, [input]);
+
   return (
     <>
-      <Search />
-      <div>
-        {countries?.map((country) => (
-          <CountryCard
-            key={country.name}
-            flag={country.flag}
-            name={country.name}
+      <div className={styles.form}>
+        <div>
+          <FaSearch className={styles.icon} />
+          <input
+            type="search"
+            placeholder="Search..."
+            onChange={(event) => {
+              setInput(event.target.value);
+            }}
           />
+        </div>
+      </div>
+
+      <div>
+        {output?.map((item: { name: string; flag: string }) => (
+          <CountryCard key={item.name} flag={item.flag} name={item.name} />
         ))}
-      </div>{' '}
+      </div>
     </>
   );
 }
